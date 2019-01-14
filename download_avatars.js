b48493fa168e020343eb5811b8f697bb364d41b4
pnolan89 <petenol@gmail.com>
2019-01-14 22:55:55 +0000

    create getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,20 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  // request.get('https://api.github.com/repos/jquery/jquery/contributors')
+  //   .on('error', function(err) {
+  //     throw err;
+  //   })
+  //   .on('response', funciton(response) {
+
+  //   })
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
