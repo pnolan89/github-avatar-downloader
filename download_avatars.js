6215dec345f837c1a016f2baa9ed429e1b08f791
pnolan89 <petenol@gmail.com>
2019-01-15 01:31:36 +0000

    function now pulls avatar URLs

--- download_avatars.js
+++ download_avatars.js
@@ -9,16 +9,35 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': secrets.GITHUB_TOKEN
+      'Authorization': "token " + secrets.GITHUB_TOKEN
     }
   };
 
   request(options, function(err, res, body) {
+    console.log(res.headers);
     cb(err, body);
   });
+
+  // request(options)
+  //   .on('error', function(err) {
+  //     throw err;
+  //   })
+  //   .on('response', function(response) {
+  //     console.log('Download started...');
+  //     console.log('Response Status Code: ' + response.statusCode);
+  //     console.log('Response Message: ' + response.statusMessage);
+  //     console.log('Content Type: ' + response.headers['content-type']);
+  //   })
+  //   .on('end', function(body) {
+
+  //     // cb(err, body);
+  //   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  var userList = JSON.parse(result);
+  userList.forEach(function(user) {
+    console.log("Result: " + user.avatar_url);
+  });
 });
